81687fd2e3f7d7c88ef7dcb0b11d740bbf961eeb
carinadidomenico <carinadidomenico@gmail.com>
2019-06-18 22:38:26 -0300

    Arrumando as pastas

--- RocketChat/src/componentes/nav/menu/Menu.js
+++ RocketChat/src/componentes/nav/menu/Menu.js
@@ -1,50 +1,57 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import './Menu.css'
+import {Link} from 'react-router-dom'
+import './menu.css'
+import '../nav.css'
 
-class LinkMenu extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      aberto: false
+class Menu extends React.Component{
+    constructor(props){
+        super(props)
+        this.state ={
+            aberto: false,
+        }
     }
-  }
 
-  handleAbreOuFecha = e => {
-    this.setState(prevState => {
-        return { aberto: !prevState.aberto }
-    })
-  }
+    handleAberto = e =>{
+        this.setState(estadoAnterior => {
+            return { aberto: !estadoAnterior.aberto}
+        })
+    }
 
 
-  render () {
-    let classesDasOpcoes = "navbar-menu__opcoes"
-    let classesDoBotao = "navbar-menu__botao"
+    render() {
+        let classeDasOpcoes = "navbar-menu__opcoes"
+        let classesDoBotao = "navbar-menu__botao"
 
-    if (this.state.aberto) {
-        classesDasOpcoes += " navbar-menu__opcoes--aberto"
-        classesDoBotao += " navbar-menu__botao--aberto"
-    } 
+        if(this.state.aberto){
+            classeDasOpcoes += " navbar-menu__opcoes--aberto"
+            classesDoBotao += " navbar-menu__botao--aberto"
+        }
 
-    return (
-      <div className="navbar-menu navbar-links">
-        <a className={classesDoBotao} onClick={this.handleAbreOuFecha}> 
-          Menu 
-        </a>
-        <ul className={classesDasOpcoes}>
-          <li>
-            <Link to="/" className="navbar-links__ativo" >Home</Link>
-          </li>
-          <li>
-            <Link to="/cadastro" className="navbar-links__ativo" >Cadastro</Link>
-          </li>
-          <li>
-            <Link to="/chat" className="navbar-links__ativo" >Chat</Link>
-          </li>
-        </ul>
-      </div>
-    )
-  }
+        return (
+            <div className='navbar-menu navbar-links'>
+                <a className={classesDoBotao} onClick={this.handleAberto}>Menu</a>
+                <ul className={classeDasOpcoes}>
+                    <li >
+                        <Link to="/" className="navbar-links__ativo">
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/cadastro" className="navbar-links__ativo">
+                            Cadastro
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/chat" className="navbar-links__ativo">
+                            Chat
+                        </Link>
+                    </li>
+                </ul>
+            </div>           
+        )
+    }
 }
+    
 
-export default LinkMenu
+
+export default Menu
