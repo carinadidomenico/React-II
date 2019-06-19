97d225e2a50d62efc9d4e466f83de1a8f456a8f5
Your Name <you@example.com>
2019-06-19 17:06:03 -0300

    Chat comentários

--- RocketChat/src/paginas/Chat.js
+++ RocketChat/src/paginas/Chat.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, {Component} from 'react'
+import Comentario from '../componentes/comentario/Comentario';
+import './chat.css'
+import {getComentarios} from '../service/base'
 
-const Chat = (props) => {
-    return(
-        <p>Chat</p>
-    )
+
+export default class Chat extends Component {
+    constructor(props) {
+        super (props)
+
+        this.state = {
+            comentarios: []
+        }
+    }
+
+    componentDidMount() {
+        getComentarios()
+        .then (response => {
+            console.log (response)
+            this.setState({
+                comentarios: response.data
+            })
+        })
+        .catch (erro => {
+            console.log (erro)
+        })
+    }
+
+    render() {
+        const {comentarios} = this.state
+        return (
+            <div className="chat">
+                {
+                    comentarios.length > 0 ? 
+                    comentarios.map((item, index) => {
+                        return (
+                            <Comentario
+                                comentario={item}
+                                key={index+'comentario'}
+                            /> 
+                        )
+                    }) :
+                    <span>Carregando comentários</span>
+                }
+            </div>
+        )
+    }
 }
-
-export default Chat
